700cf618a11aff86496d7fdea107232a73757b27
AkankshaNangare <akankshanangare5055@gmail.com>
2024-08-24 12:20:42 +0530

    second commit

---
+++ src/Components/About.js
@@ -0,0 +1,56 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div>
+      <div className="container my-5 justify-content-center">
+        <div className="row g-5 justify-content-center">
+          <h2 style={{ textAlign: "center" }}>About Us</h2>
+          <br></br>
+          <h4>
+          Welcome to Trend Shop - Where Fashion Meets Style!
+
+    </h4>
+          <br></br>
+          <p>
+          Our Story
+At Trend Shop, fashion isn't just a passion; it's our way of life. Our journey began in [Year] when a group of fashion enthusiasts decided to turn their love for style into a destination where everyone can discover the latest trends, express their individuality, and feel confident in their choices.
+
+Our Mission
+Empowering Your Style Journey: Our mission is to empower individuals to express their unique style with confidence. We believe that fashion should be inclusive, accessible, and a source of inspiration for all.
+
+What Sets Us Apart
+Curated Selection: We curate a carefully selected collection of the latest trends, ensuring you have access to high-quality fashion that stands out.
+Our Story
+At Trend Shop, fashion isn't just a passion; it's our way of life. Our journey began in [Year] when a group of fashion enthusiasts decided to turn their love for style into a destination where everyone can discover the latest trends, express their individuality, and feel confident in their choices.
+
+Our Mission
+Empowering Your Style Journey: Our mission is to empower individuals to express their unique style with confidence. We believe that fashion should be inclusive, accessible, and a source of inspiration for all.
+
+What Sets Us Apart
+Curated Selection: We curate a carefully selected collection of the latest trends, ensuring you have access to high-quality fashion that stands out.
+
+Diverse Styles: From classic elegance to street style, our range covers a diverse spectrum of styles to cater to every taste.
+
+Sustainability: We're committed to sustainability. Our eco-friendly initiatives and ethical sourcing reflect our dedication to a better future.
+
+Customer-Centric: Your satisfaction is our priority. Our customer support team is here to assist you at every step of your shopping journey.
+      
+Diverse Styles: From classic elegance to street style, our range covers a diverse spFectrum of styles to cater to every taste.
+
+Sustainability: We're committed to sustainability. Our eco-friendly initiatives and ethical sourcing reflect our dedication to a better future.
+
+Customer-Centric: Your satisfaction is our priority. Our customer support team is here to assist you at every step of your shopping journey.
+      
+          </p>
+          <br></br>
+          <button className="btn btn-outline-warning  w-25">
+            Get in Touch
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/Redux/Reducer/handleCart.js
@@ -0,0 +1,18 @@
+const cart = [];
+
+const handleCart = (state = cart, action) => {
+  switch (action.type) {
+    case "ADDITEM":
+      return [...state, action.payload];
+
+    case "DELITEM":
+      return (state = state.filter((x) => {
+        return x.id !== action.payload.id;
+      }));
+
+    default:
+      return state;
+  }
+};
+
+export default handleCart;
